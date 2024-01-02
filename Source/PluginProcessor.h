9db9c698230211378475c051088f02df22d11f9d
Weston Clark <westoclark@gmail.com>
2024-01-02 12:11:21 -0800

    Connect LowCut Fitler Params to LowCut Filter

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -10,12 +10,19 @@
 
 #include <JuceHeader.h>
 
+enum Slope
+{
+    Slope_12,
+    Slope_24,
+    Slope_36,
+    Slope_48
+};
 
 struct ChainSettings
 {
     float peakFreq {0}, peakGainInDecibels {0}, peakQuality {1.f};
     float lowCutFreq {0}, highCutFreq {0};
-    int lowCutSlope {0}, highCutSlope {0};
+    Slope lowCutSlope {Slope::Slope_12}, highCutSlope {Slope::Slope_12};
 };
 
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
@@ -70,9 +77,7 @@ private:
     
     // Create aliases to simplify refrencing dsp
     using Filter = juce::dsp::IIR::Filter<float>;
-    
     using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
-    
     using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
 
     MonoChain leftChain, rightChain;
